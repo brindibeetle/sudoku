3c0f1e7381bd934cd4cdcec14310b858ee3ca1a2
Emile <brindi.beetle@gmail.com>
2019-05-13 21:08:09 +0200

    kleine wijzigingen

--- Sudoku/src/main/java/brindi/beetle/sudoku/SudokuForm.java
+++ Sudoku/src/main/java/brindi/beetle/sudoku/SudokuForm.java
@@ -3,7 +3,6 @@ package brindi.beetle.sudoku;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.management.Query;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
